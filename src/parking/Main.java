61b22ac752d04228aa41d14544e3702ad085a3b9
menuHwang <hmin0206@naver.com>
2023-06-27 18:26:44 +0900

    입차, 출차 기능

---
+++ src/parking/Main.java
@@ -0,0 +1,9 @@
+package parking;
+
+public class Main {
+    public static void main(String[] args) {
+        Parking parking = new Parking();
+        parking.in();
+        parking.out();
+    }
+}
---
+++ src/parking/Parking.java
@@ -0,0 +1,12 @@
+package parking;
+
+public class Parking {
+
+    public void in() {
+        System.out.println("입차");
+    }
+
+    public void out() {
+        System.out.println("출차");
+    }
+}
